08e48b618a6d18d6f699edb2e67dc3a7f8624e23
Evan Grim <evan@mirgnave.com>
2015-02-25 14:47:15 -0600

    Vanilla sentry.conf.py (as generated by `sentry init`)

--- sentry.conf.py
+++ sentry.conf.py
@@ -1,176 +1,209 @@
-import logging
-import os
-import sys
 
+# This file is just Python, with a touch of Django which means you
+# you can inherit and tweak settings to your hearts content.
 from sentry.conf.server import *
 
-ROOT = os.path.dirname(__file__)
+import os.path
 
-sys.path.append(ROOT)
+CONF_ROOT = os.path.dirname(__file__)
 
-import dj_database_url
-DATABASES = {'default': dj_database_url.config()}
-
-
-# Sentry configuration
-# --------------------
-
-SENTRY_KEY = os.environ.get('SENTRY_KEY')
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
-# Set this to false to require authentication
-SENTRY_PUBLIC = False
-
-SENTRY_WEB_HOST = '0.0.0.0'
-SENTRY_WEB_PORT = int(os.environ.get('PORT', 9000))
-SENTRY_WEB_OPTIONS = {
-    'workers': 3,
-    'worker_class': 'gevent',
-}
-
-SENTRY_URL_PREFIX = os.environ.get('SENTRY_URL_PREFIX', '')
-
-
-# Caching
-# -------
-
-CACHES = {
+DATABASES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        # You can swap out the engine for MySQL easily by changing this value
+        # to ``django.db.backends.mysql`` or to PostgreSQL with
+        # ``django.db.backends.postgresql_psycopg2``
+
+        # If you change this, you'll also need to install the appropriate python
+        # package: psycopg2 (Postgres) or mysql-python
+        'ENGINE': 'django.db.backends.sqlite3',
+
+        'NAME': os.path.join(CONF_ROOT, 'sentry.db'),
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
+# You should not change this setting after your database has been created
+# unless you have altered all schemas first
+SENTRY_USE_BIG_INTS = True
 
-# Email configuration
-# -------------------
+# If you're expecting any kind of real traffic on Sentry, we highly recommend
+# configuring the CACHES and Redis settings
 
-if 'SENDGRID_USERNAME' in os.environ:
-    EMAIL_HOST = 'smtp.sendgrid.net'
-    EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
-    EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
-elif 'MANDRILL_USERNAME' in os.environ:
-    EMAIL_HOST = 'smtp.mandrillapp.com'
-    EMAIL_HOST_USER = os.environ.get('MANDRILL_USERNAME')
-    EMAIL_HOST_PASSWORD = os.environ.get('MANDRILL_APIKEY')
+#############
+## General ##
+#############
 
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# The administrative email for this installation.
+# Note: This will be reported back to getsentry.com as the point of contact. See
+# the beacon documentation for more information.
+SENTRY_ADMIN_EMAIL = 'your.name@example.com'
 
-# Disable the default admins (for email)
-ADMINS = ()
+###########
+## Redis ##
+###########
 
-# Set Sentry's ADMINS to a raw list of email addresses
-SENTRY_ADMINS = os.environ.get('ADMINS', '').split(',')
+# Generic Redis configuration used as defaults for various things including:
+# Buffers, Quotas, TSDB
 
-# The threshold level to restrict emails to.
-SENTRY_MAIL_LEVEL = logging.WARNING
+SENTRY_REDIS_OPTIONS = {
+    'hosts': {
+        0: {
+            'host': '127.0.0.1',
+            'port': 6379,
+        }
+    }
+}
 
-# The prefix to apply to outgoing emails.
-EMAIL_SUBJECT_PREFIX = '[Sentry] '
+###########
+## Cache ##
+###########
 
-# The reply-to email address for outgoing mail.
-SERVER_EMAIL = os.environ.get('SERVER_EMAIL', 'root@localhost')
-
-DEFAULT_FROM_EMAIL = os.environ.get(
-    'DEFAULT_FROM_EMAIL',
-    'webmaster@localhost'
-)
-
-
-# Security
-# --------
-
-INSTALLED_APPS += ('djangosecure',)
-MIDDLEWARE_CLASSES += ('djangosecure.middleware.SecurityMiddleware',)
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Whether to use HTTPOnly flag on the session cookie. If this is set to `True`,
-# client-side JavaScript will not to be able to access the session cookie.
-SESSION_COOKIE_HTTPONLY = True
-
-# Whether to use a secure cookie for the session cookie.  If this is set to
-# `True`, the cookie will be marked as "secure," which means browsers may
-# ensure that the cookie is only sent under an HTTPS connection.
-SESSION_COOKIE_SECURE = True
-
-# If set to `True`, causes `SecurityMiddleware` to set the
-# `X-Content-Type-Options: nosniff` header on all responses that do not already
-# have that header.
-SECURE_CONTENT_TYPE_NOSNIFF = True
-
-# If set to `True`, causes `SecurityMiddleware` to set the
-# `X-XSS-Protection: 1; mode=block` header on all responses that do not already
-# have that header.
-SECURE_BROWSER_XSS_FILTER = True
-
-# If set to `True`, causes `SecurityMiddleware` to set the `X-Frame-Options:
-# DENY` header on all responses that do not already have that header
-SECURE_FRAME_DENY = True
-
-# If set to a non-zero integer value, causes `SecurityMiddleware` to set the
-# HTTP Strict Transport Security header on all responses that do not already
-# have that header.
-SECURE_HSTS_SECONDS = 31536000
-
-# If `True`, causes `SecurityMiddleware` to add the ``includeSubDomains`` tag
-# to the HTTP Strict Transport Security header.
+# If you wish to use memcached, install the dependencies and adjust the config
+# as shown:
 #
-# Has no effect unless ``SECURE_HSTS_SECONDS`` is set to a non-zero value.
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-
-# If set to True, causes `SecurityMiddleware` to redirect all non-HTTPS
-# requests to HTTPS
-SECURE_SSL_REDIRECT = True
-
-
-# Bcrypt
-# ------
-
-INSTALLED_APPS += ('django_bcrypt',)
-
-# Enables bcrypt password migration on a ``check_password()`` call.
+#   pip install python-memcached
 #
-# The hash is also migrated when ``BCRYPT_ROUNDS`` changes.
-BCRYPT_MIGRATE = True
+# CACHES = {
+#     'default': {
+#         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#         'LOCATION': ['127.0.0.1:11211'],
+#     }
+# }
+#
+# SENTRY_CACHE = 'sentry.cache.django.DjangoCache'
 
+SENTRY_CACHE = 'sentry.cache.redis.RedisCache'
 
-# Social Auth
-# -----------
+###########
+## Queue ##
+###########
 
-SOCIAL_AUTH_CREATE_USERS = 'SOCIAL_AUTH_CREATE_USERS' in os.environ
+# See http://sentry.readthedocs.org/en/latest/queue/index.html for more
+# information on configuring your queue broker and workers. Sentry relies
+# on a Python framework called Celery to manage queues.
 
+CELERY_ALWAYS_EAGER = False
+BROKER_URL = 'redis://localhost:6379'
 
-# Twitter
-# -------
-TWITTER_CONSUMER_KEY = os.environ.get('TWITTER_CONSUMER_KEY')
-TWITTER_CONSUMER_SECRET = os.environ.get('TWITTER_CONSUMER_SECRET')
+#################
+## Rate Limits ##
+#################
 
+SENTRY_RATELIMITER = 'sentry.ratelimits.redis.RedisRateLimiter'
 
-# Facebook
-# --------
+####################
+## Update Buffers ##
+####################
 
-FACEBOOK_APP_ID = os.environ.get('FACEBOOK_APP_ID')
-FACEBOOK_API_SECRET = os.environ.get('FACEBOOK_API_SECRET')
+# Buffers (combined with queueing) act as an intermediate layer between the
+# database and the storage API. They will greatly improve efficiency on large
+# numbers of the same events being sent to the API in a short amount of time.
+# (read: if you send any kind of real data to Sentry, you should enable buffers)
 
+SENTRY_BUFFER = 'sentry.buffer.redis.RedisBuffer'
 
-# Google
-# ------
+############
+## Quotas ##
+############
 
-GOOGLE_OAUTH2_CLIENT_ID = os.environ.get('GOOGLE_OAUTH2_CLIENT_ID')
-GOOGLE_OAUTH2_CLIENT_SECRET = os.environ.get('GOOGLE_OAUTH2_CLIENT_SECRET')
+# Quotas allow you to rate limit individual projects or the Sentry install as
+# a whole.
 
+SENTRY_QUOTAS = 'sentry.quotas.redis.RedisQuota'
 
-# GitHub
-# ------
+##########
+## TSDB ##
+##########
 
-GITHUB_APP_ID = os.environ.get('GITHUB_APP_ID')
-GITHUB_API_SECRET = os.environ.get('GITHUB_API_SECRET')
-GITHUB_EXTENDED_PERMISSIONS = ['repo']
+# The TSDB is used for building charts as well as making things like per-rate
+# alerts possible.
 
+SENTRY_TSDB = 'sentry.tsdb.redis.RedisTSDB'
 
-# Bitbucket
-# ---------
+##################
+## File storage ##
+##################
 
-BITBUCKET_CONSUMER_KEY = os.environ.get('BITBUCKET_CONSUMER_KEY')
-BITBUCKET_CONSUMER_SECRET = os.environ.get('BITBUCKET_CONSUMER_SECRET')
+# Any Django storage backend is compatible with Sentry. For more solutions see
+# the django-storages package: https://django-storages.readthedocs.org/en/latest/
+
+SENTRY_FILESTORE = 'django.core.files.storage.FileSystemStorage'
+SENTRY_FILESTORE_OPTIONS = {
+    'location': '/tmp/sentry-files',
+}
+
+################
+## Web Server ##
+################
+
+# You MUST configure the absolute URI root for Sentry:
+SENTRY_URL_PREFIX = 'http://sentry.example.com'  # No trailing slash!
+
+# If you're using a reverse proxy, you should enable the X-Forwarded-Proto
+# header and uncomment the following settings
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+SENTRY_WEB_HOST = '0.0.0.0'
+SENTRY_WEB_PORT = 9000
+SENTRY_WEB_OPTIONS = {
+    # 'workers': 3,  # the number of gunicorn workers
+    # 'secure_scheme_headers': {'X-FORWARDED-PROTO': 'https'},
+}
+
+#################
+## Mail Server ##
+#################
+
+# For more information check Django's documentation:
+#  https://docs.djangoproject.com/en/1.3/topics/email/?from=olddocs#e-mail-backends
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = ''
+EMAIL_PORT = 25
+EMAIL_USE_TLS = False
+
+# The email address to send on behalf of
+SERVER_EMAIL = 'root@localhost'
+
+# If you're using mailgun for inbound mail, set your API key and configure a
+# route to forward to /api/hooks/mailgun/inbound/
+MAILGUN_API_KEY = ''
+
+###########
+## etc. ##
+###########
+
+# If this file ever becomes compromised, it's important to regenerate your SECRET_KEY
+# Changing this value will result in all current sessions being invalidated
+SECRET_KEY = 'heZ7qAQNIFxF0ECQCp5LxrGrUm7dHLGRL9Wr2xt1d4k7/yVFQ4PutQ=='
+
+# http://twitter.com/apps/new
+# It's important that input a callback URL, even if its useless. We have no idea why, consult Twitter.
+TWITTER_CONSUMER_KEY = ''
+TWITTER_CONSUMER_SECRET = ''
+
+# http://developers.facebook.com/setup/
+FACEBOOK_APP_ID = ''
+FACEBOOK_API_SECRET = ''
+
+# http://code.google.com/apis/accounts/docs/OAuth2.html#Registering
+GOOGLE_OAUTH2_CLIENT_ID = ''
+GOOGLE_OAUTH2_CLIENT_SECRET = ''
+
+# https://github.com/settings/applications/new
+GITHUB_APP_ID = ''
+GITHUB_API_SECRET = ''
+
+# https://trello.com/1/appKey/generate
+TRELLO_API_KEY = ''
+TRELLO_API_SECRET = ''
+
+# https://confluence.atlassian.com/display/BITBUCKET/OAuth+Consumers
+BITBUCKET_CONSUMER_KEY = ''
+BITBUCKET_CONSUMER_SECRET = ''
